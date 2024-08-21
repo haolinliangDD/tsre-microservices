11cb275b4cc50386343a4b2af67e7d522ce4c533
lucarelli111 <alessandro.lucarelli@datadoghq.com>
2024-08-21 13:57:47 -0400

    Added logging to paymentservice for SRE Workshop

--- src/paymentservice/src/main/java/com/hipstershop/paymentservicejava/PaymentServiceImpl.java
+++ src/paymentservice/src/main/java/com/hipstershop/paymentservicejava/PaymentServiceImpl.java
@@ -39,8 +39,10 @@ public class PaymentServiceImpl extends PaymentServiceGrpc.PaymentServiceImplBas
             String transactionId = "none";
             try {
                 transactionId = controller.clearPayment(request);
+                log.info("Payment transaction processed successfully. Transaction ID: " + transactionId);
             } catch(Exception e) {
-                this.log.log(Level.SEVERE, e.getClass().getName() + " occurred. Cannot process payment transaction.");
+                // this.log.log(Level.SEVERE, e.getClass().getName() + " occurred. Cannot process payment transaction."); -- changing logger so it reports to Datadog
+                log.severe(e.getClass().getName() + " occurred. Cannot process payment transaction.");
                 com.google.rpc.Status status = com.google.rpc.Status.newBuilder()
                 .setCode(com.google.rpc.Code.UNAVAILABLE_VALUE)
                 .setMessage(e.getClass().getName())
